a294b3dec332b151788dd5d41bbdef9e2a58b5d7
Mike Mathew <m2mathew@me.com>
2015-10-19 15:27:13 -0500

    Tax is worked out for WI, but no others, as it should be

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,54 @@
+/*
+ *  Tax Calculator
+ *
+ *	#14 from the book "Exercises for Programmers"
+ *
+ */
+
 'use strict';
 
+import $ from 'jquery';
+
+$(document).ready(function() {
+
+	let amount, state, tax, total, newTax = null;
+
+	// use an if statement, not an else statement
+
+	// round money up to nearest cent
+
+	// use a simple output statement at the end to display results
+
+	let $amount = $('#amount');
+	let $state = $('#state');
+	let $button = $('button');
+	let $subtotal = $('#subtotal');
+	let $tax = $('#tax');
+	let $total = $('#total');
+
+	$button.on('click', function(e) {
+		e.preventDefault();
+
+		amount = $amount.val();
+		state = $state.val().toUpperCase();
+
+		tax = 5.5;
+		newTax = ((tax/100) * amount);
+		let newTax = parseFloat(newTax);
+
+		let total = parseFloat(amount);
+
+		if (state === 'WI') {
+			total = total + newTax;
+
+			$subtotal.text('Original Amount: $' + amount);
+			$tax.text('Tax rate: ' + tax + '%');
+			$total.text('Total due: $' + total.toFixed(2));
+		}
+
+		if (state !== 'WI') {
+			$total.text('Total due: $' + amount);
+		}
+
+	});
+});
